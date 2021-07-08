967ecc87751e60667caa6cc6002be7d932e2ec31
JVCanada <jv.canada1@gmail.com>
2021-07-08 14:49:57 -0300

    exercicio 1.2

--- 01-exercicio.rb
+++ 01-exercicio.rb
@@ -1,14 +1,14 @@
 puts "quantas horas há em um ano?"
-puts "8760"
+puts 365*24
 
 puts "quantos minutos há em uma década?"
-puts "52560000 minutos......."
+puts 1440*3650
 
 puts "qual é a sua idade em segundos?"
-puts "635441242 segundos"
+puts 20*3154000.00
 
 puts "quantos chocolates você pretende comer na vida?"
-puts "Mais de 300"
+puts 120000/365 
 
 puts "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
-puts "Aproximadamente 40 anos"
+puts 31536000.00 / 1232000.00
