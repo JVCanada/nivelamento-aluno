08951d7be8e53dd7aa0df7c4e457d3e9e1df74dc
JVCanada <jv.canada1@gmail.com>
2021-07-08 15:25:01 -0300

    exercicio 1.4

--- 01-exercicio.rb
+++ 01-exercicio.rb
@@ -8,7 +8,7 @@ puts "qual é a sua idade em segundos?"
 puts 20*3154000.00
 
 puts "quantos chocolates você pretende comer na vida?"
-puts (1500*80) / 365
+puts (18250*80) / 365
 
 puts "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
 puts 31536000.00 / 1232000.00
