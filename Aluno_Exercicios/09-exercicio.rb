681afe7cfbfa0a4694ceff16a316dcdfe6da8160
JVCanada <jv.canada1@gmail.com>
2021-07-16 15:11:53 -0300

    exercício 9 versão 2

--- Aluno_Exercicios/09-exercicio.rb
+++ Aluno_Exercicios/09-exercicio.rb
@@ -44,75 +44,10 @@ if chute.to_i == numero
     puts "O seu número de tentativas foi: " + i.to_s
 end
 
-
-
 # # Exercício 09
 # # A partir da versão 2 do jogo de adivinhação, faça uma nova versão em que o jogador receba a informação, 
 # # após chutar o número, se o chute foi maior ou menor que o número pensado. 
 # # A seguir, ele ganha uma nova tentativa, de modo que o jogo somente vai terminar quando ele acertar.
 # # Ao final, após acertar, informe o número de tentativas que o jogador teve para acertar.
 # # Observação: valide o chute do jogador, ignorando a tentativa caso ele informe um numero invalido.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# numero = rand(0..100)
-# # puts "(" + numero.to_s + ")"
-
-# chute = gets.chomp
-
-# # puts chute
-# # puts chute.to_i
-
-# if chute == "0" || (chute.to_i >= 1 && chute.to_i <= 100)
-#     if chute.to_i > numero
-#         puts "Acertou! Seu número é maior!"
-#     elsif chute.to_i < numero
-#         puts "Acertou! Seu número é menor!"
-#     else
-#         while chute.to_i != numero
-#             puts "Voce errou. Tente novamente!"
-#         end
-#     end
-# else
-#     puts "Chute inválido!"
-# end
-
 
