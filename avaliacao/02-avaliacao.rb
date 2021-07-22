6854f818c475681bb2eccf0ba8ba49c1985796b7
JVCanada <jv.canada1@gmail.com>
2021-07-22 15:00:26 -0300

    exercício 2 da avaliação

--- avaliacao/02-avaliacao.rb
+++ avaliacao/02-avaliacao.rb
@@ -7,8 +7,6 @@
 # Pois o resultado é igual a: (1*10) + (2*20) + (3*30)
 # Obs.: Valide se os 2 arrays têm o mesmo tamanho. Caso não tiverem, a função deve retornar nil.
 
-# 15:20
-
 def multiplica_arrays(array1,array2)
     resultado = 0
     if array1.size == array2.size
